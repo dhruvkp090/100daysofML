f550c9c1c6300d43194d5204f948023790dcd3a8
dhruvkp090 <45363265+dhruvkp090@users.noreply.github.com>
2019-08-24 20:19:18 +0530

    Add files via upload

---
+++ similarity.py
@@ -0,0 +1,43 @@
+'''
+Author: Dhruv Kumar Patwari (dhruvk090@gmail.com)
+
+Description: This file is used to check the similarity between two sentences.
+
+
+'''
+
+import nltk
+import numpy as np
+import random
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+lemmer = nltk.stem.WordNetLemmatizer()
+
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+def similarity(input_question, databade_question):
+    rawData = input_question.lower()
+    sent_tokens = nltk.sent_tokenize(rawData)
+    sent_tokens.append(databade_question.lower())
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf < 0.2):
+        return False
+    else:
+        return True
