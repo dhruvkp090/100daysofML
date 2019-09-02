d49847c93e13629a1d7f9cc5a8a6d43ad71647bc
dhruvkp090 <45363265+dhruvkp090@users.noreply.github.com>
2019-09-02 17:20:23 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from base import db
-
-class FAQ(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(128), index=True, unique=True)
-    answer = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<Question {}>'.format(self.question)
