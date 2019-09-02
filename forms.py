6f80a6cf242c0d4401fb5235f35eb5932586675e
dhruvkp090 <45363265+dhruvkp090@users.noreply.github.com>
2019-09-02 17:20:07 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class FAQform(FlaskForm):
-    Question = StringField('Question', validators=[DataRequired()])
-    Answer = StringField('Answer', validators=[DataRequired()])
-    submit = SubmitField('Add')
